a133d1ab8074c4fd30e4e5f7b3f522d852a0250d
Kenneth Stallworth <kennethstallworth@hotmail.com>
2026-02-03 22:53:36 -0500

    update task model mappers

--- src/main/java/com/project/projectmanager/mappers/impl/TaskMapperImpl.java
+++ src/main/java/com/project/projectmanager/mappers/impl/TaskMapperImpl.java
@@ -2,32 +2,30 @@
 
 
 
-// import    com.project.projectmanager.domain.dto
-// import    com.project.projectmanager.domain
-// import    com.project.projectmanager.mappers.Mapper;
-// import org.modelMapper.ModelMapper;
+ import    com.project.projectmanager.domain.dto.TaskDto;
+ import    com.project.projectmanager.domain.TaskEntity;
+ import    com.project.projectmanager.mappers.Mapper;
+ import    org.modelmapper.ModelMapper;
 
 
 
 // @Component
-public class TaskMapperImpl {}
-
-
-// implements Mapper<domain object, Dto>{
+public class TaskMapperImpl implements Mapper<TaskEntity, TaskDto>{
 	
-//     private ModelMapper modelMapper;
+     private ModelMapper modelMapper;
 
-//     public MapperImpl(ModelMapper _modelMapper){
-//         this.modelMapper = _modelMapper; 
-//     }
-// 	@Override
-// 	public Dto mapTo(Domin object){
-// 	    return modelMapper.map(domainObject, Dto.class);
-// 	}
+     public TaskMapperImpl(ModelMapper _modelMapper){
+         this.modelMapper = _modelMapper; 
+     }
+     
+ 	@Override
+ 	public TaskDto mapTo(TaskEntity task_entity){
+ 	    return modelMapper.map(task_entity, TaskDto.class);
+ 	}
 
 
-// 	@Override 
-// 	public domainObject mapFrom(DTO dto){
-// 	   return modelMapper.map(Dto, domainObject);
-// 	}
-// }
+	 	@Override 
+	 	public TaskEntity mapFrom(TaskDto task_dto){
+	 	   return modelMapper.map(task_dto, TaskEntity.class);
+	 	}
+ }
